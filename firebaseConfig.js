c8f15f143a55c964b10e1e2e40c5599a02353bea
Jadok <jadoksenpai12@gmail.com>
2026-04-22 00:52:53 +0800

    .

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,7 +3,7 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBbAofFm_8ncGMRB0X2szfKC9TMvmcrzXo",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
   authDomain: "parseit2-4b26d.firebaseapp.com",
   projectId: "parseit2-4b26d",
   storageBucket: "parseit2-4b26d.firebasestorage.app",
@@ -17,3 +17,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { app, auth, db };
+
