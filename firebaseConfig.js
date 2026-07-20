3baf2f9bf4439af56ff821d0f2960dd60eb45ba5
Jadok <jadoksenpai12@gmail.com>
2026-07-20 19:18:39 +0800

    change firebaseConfig

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -10,11 +10,11 @@ import { Platform } from "react-native";
 
 const firebaseConfig = {
   apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
-  authDomain: "parseit2-4b26d.firebaseapp.com",
-  projectId: "parseit2-4b26d",
-  storageBucket: "parseit2-4b26d.firebasestorage.app",
-  messagingSenderId: "808675748328",
-  appId: "1:808675748328:web:040c0b704a93716ec79c5d",
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase App
