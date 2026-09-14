992366efa373bf06f04fc9ecf1e6a7754870e666
Jadok <jadoksenpai12@gmail.com>
2026-09-14 23:40:58 +0800

    ..

--- src/Final_Admin_Components/Header.tsx
+++ src/Final_Admin_Components/Header.tsx
@@ -411,7 +411,7 @@ export default function Header({
         <View style={styles.row}>
           <View style={styles.left}>
             <Image
-              source={require("../../assets/images/BSITLOGO.png")}
+              source={require("../../assets/images/logo.png")}
               style={styles.logo}
               resizeMode="contain"
             />
@@ -476,7 +476,7 @@ export default function Header({
               </TouchableOpacity>
 
               <Image
-                source={require("../../assets/images/BSITLOGO.png")}
+                source={require("../../assets/images/logo.png")}
                 style={styles.logo}
                 resizeMode="contain"
               />
