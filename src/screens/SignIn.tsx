a538b2baec5e20e71b488a61c928e3d0a88c5a1b
Jadok <jadoksenpai12@gmail.com>
2026-09-20 16:43:36 +0800

    .

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -2100,7 +2100,7 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: '#D32F2F',
   },
-  mFooter: { paddingTop: 32 },
+  mFooter: { paddingTop: 35 },
   mPrimaryButton: {
     width: '100%',
     height: 54,
